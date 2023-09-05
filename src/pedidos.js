16111a346777df20b9e0a66d72e87538f3d3096a
Christian <116993834+christiancordeiro@users.noreply.github.com>
2023-09-05 18:04:02 -0300

    Update pedidos.js

--- src/pedidos.js
+++ src/pedidos.js
@@ -1,4 +1,4 @@
-import { lerLocalStorage, desenharProdutoCarrinhoSimples } from "./utilidades.js";
+import { lerLocalStorage, desenharProdutoCarrinhoSimples } from "./src/utilidades.js";
 
 function criarPedidoHistorico(pedidoComData) {
     const elementoPedido = `
